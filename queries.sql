f1a6932588daceab357515b971407c52152a83c8
Helina <67068750+helina-s@users.noreply.github.com>
2020-11-02 20:25:59 -0500

    edited merged_data, csv and queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,14 @@
--- drop table merged_data 
 create table merged_data (
 	country_code varchar not null,
 	country varchar not null,
 	year varchar not null,
 	start_date date not null,
-	event_name varchar,
+	event_name varchar not null,
 	hazard_category varchar not null,
 	hazard_type varchar not null,
-	new_displacement varchar,
+	new_displacement varchar not null,
 	country_2D varchar not null,
 	continent varchar not null,
-	population varchar
+	population varchar not null
 );
 select * from merged_data
