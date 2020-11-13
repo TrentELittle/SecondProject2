50808fb1bb30ff5f8cc386de465d81ec473897fc
Trent Litte <trent.e.little@gmail.com>
2020-11-13 15:20:35 -0500

    .

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
 import numpy as np
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+
 
 from flask import Flask, jsonify, render_template, url_for, send_from_directory
 
